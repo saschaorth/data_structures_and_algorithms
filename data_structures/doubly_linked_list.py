89c3722a69f1ff40d32295198023c39829504f06
saschaorth <sascha.orth@web.de>
2023-11-27 21:41:25 +0100

    Create insert method

--- data_structures/doubly_linked_list.py
+++ data_structures/doubly_linked_list.py
@@ -1,11 +1,11 @@
-class Node():
+class Node:
     def __init__(self, value):
         self.value = value
         self.previous = None
         self.next = None
 
 
-class DoublyLinkedList():
+class DoublyLinkedList:
     def __init__(self, value):
         self.head = Node(value)
         self.tail = self.head
@@ -23,6 +23,39 @@ class DoublyLinkedList():
         new_node.next = self.head
         new_node.previous = None
         self.head = new_node
+        self.length += 1
+
+    def get_node(self, index, start_from="head"):
+        if start_from not in ["head", "tail"]:
+            raise Exception("start_from must be head or tail")
+        current_node = getattr(self, start_from)
+        counter = 0
+
+        while counter < index:
+            if start_from == "head":
+                current_node = current_node.next
+            else:
+                current_node = current_node.previous
+            counter += 1
+        return current_node
+
+    def insert(self, index, value):
+        if (index + 1) > self.length:
+            self.append(value)
+            return
+
+        if index == 0:
+            self.prepend(value)
+            return
+
+        new_node = Node(value)
+        if (index + 1) > (self.length / 2):
+            leading_node = self.get_node(self.length - index, start_from="tail")
+        else:
+            leading_node = self.get_node(index - 1)
+        following_node = leading_node.next
+        new_node.next = following_node
+        leading_node.next = new_node
         self.length += 1
 
     def __repr__(self):
