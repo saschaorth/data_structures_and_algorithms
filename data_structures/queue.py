8ba87ba379d8efd6238bd78f2ee8e58a3ef8f9f4
saschaorth <sascha.orth@web.de>
2023-12-01 17:38:50 +0100

    Create queue based on a linke list

---
+++ data_structures/queue.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Queue:
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.length = 0
+
+    def peek(self):
+        if self.length == 0:
+            return
+        return self.first.value
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.first = new_node
+            self.last = self.first
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+
+    def dequeue(self):
+        if self.length == 0:
+            return
+        new_first = self.first.next
+        self.first = new_first
+        self.length -= 1
