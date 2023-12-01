db194724266d169f397152787697b9c554aa8df2
saschaorth <sascha.orth@web.de>
2023-12-01 16:55:31 +0100

    Remove redundant code from LinkedListStack

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -6,27 +6,23 @@ class Node:
 
 class LinkedListStack:
     top = None
-    bottom = None
     length = 0
 
     def peek(self):
+        if self.length == 0:
+            return None
         return self.top.value
 
     def push(self, value):
         new_node = Node(value)
         if not self.top:
             self.top = new_node
-            self.bottom = new_node
         else:
             new_node.next = self.top
             self.top = new_node
         self.length += 1
 
     def pop(self):
-        if self.length == 1:
-            self.bottom = None
-        if not self.bottom:
-            return
         new_top = self.top.next
         self.top = new_top
         self.length -= 1
