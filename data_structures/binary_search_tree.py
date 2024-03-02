367a21583caae5c751e468af43561d968eed2598
saschaorth <sascha.orth@web.de>
2024-03-02 17:29:07 +0100

    Add remove method

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -43,3 +43,42 @@ class BinarySearchTree:
             else:
                 current_node = current_node.left
         return
+
+    def _reconnect(self, value, parent_node=None, child_node=None):
+        if not parent_node:
+            self.root = child_node
+        elif parent_node.value > value:
+            parent_node.left = child_node
+        else:
+            parent_node.right = child_node
+
+    def remove(self, value):
+        if not self.root:
+            return
+
+        current_node = self.root
+        parent_node = None
+        while current_node:
+            if value == current_node.value:
+                if not current_node.right and not current_node.left:  # no children
+                    self._reconnect(value, parent_node)
+                elif not current_node.right:  # only left child
+                    self._reconnect(value, parent_node, current_node.left)
+                elif current_node.right.left:  # right child has left child
+                    to_delete_node = current_node.right.left
+                    self.remove(to_delete_node.value)  # remove the left child of the right child
+                    to_delete_node.left = current_node.left
+                    to_delete_node.right = current_node.right
+                    self._reconnect(value, parent_node, to_delete_node)
+                else:  # only right child
+                    current_node.right.left = current_node.left
+                    self._reconnect(value, parent_node, current_node.right)
+                break
+
+            elif value > current_node.value:
+                parent_node = current_node
+                current_node = current_node.right
+            else:
+                parent_node = current_node
+                current_node = current_node.left
+        return
